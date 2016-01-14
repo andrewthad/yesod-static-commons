8697a0860cf7f521eeeab837d60fe2996d2992de
Andrew Martin <amartin@layer3com.com>
2016-01-14 12:53:50 -0500

    Add persistent instances to color

--- src/Yesod/Form/Fields/Bootstrap/Types.hs
+++ src/Yesod/Form/Fields/Bootstrap/Types.hs
@@ -1,6 +1,9 @@
 module Yesod.Form.Fields.Bootstrap.Types where
 
-import           Data.Text (Text)
+import           Data.Text            (Text)
+import           Database.Persist     (PersistField)
+import           Database.Persist.Sql (PersistFieldSql)
 
 newtype Color = Color { getColor :: Text }
+  deriving (PersistField, PersistFieldSql)
 
